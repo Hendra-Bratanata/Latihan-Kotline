90347fb6ad48f2a70bb5323a64e7dc4470e7ee5e
Hendra-Bratanata <Hendrabratanata88@gmail.com>
2018-09-04 17:46:53 +0700

    finis erro dan penambahan untuk img view anko layot dan info

--- app/src/main/java/com/example/ares/footballclub/MainActivity.kt
+++ app/src/main/java/com/example/ares/footballclub/MainActivity.kt
@@ -17,18 +17,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         initData()
         clubList.layoutManager = LinearLayoutManager(this)
-        clubList.adapter = Adapter(this,items){
-//            val toast = Toast.makeText(applicationContext, it.name,Toast.LENGTH_SHORT)
-//            toast.show()
+        clubList.adapter = Adapter(this, items) {
+            //            val toast = Toast.makeText(applicationContext, it.name,Toast.LENGTH_SHORT)
+//            toast.show
+          startActivity<Main2Activity>("name" to it.name,"gambar" to it.image,"info" to it.info)
+
+
+        }
     }
 
     private fun initData(){
+        val info = resources.getStringArray(R.array.club_info)
         val name = resources.getStringArray(R.array.club_name)
         val gambar = resources.obtainTypedArray(R.array.club_image)
         items.clear()
 
         for (i in name.indices){
-            items.add(Item(name[i],gambar.getResourceId(i,0)))
+            items.add(Item(name[i],gambar.getResourceId(i,0),info[i]))
         }
         gambar.recycle()
     }
